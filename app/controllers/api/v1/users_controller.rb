72de37a07ff39e29f65fcf22abf1c1432a9089ee
Pedro Guerreiro <pmoreiraguerreiro@gmail.com>
2022-07-11 14:15:53 +0100

    Add user_params method to users controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,4 +2,10 @@ class Api::UsersController < ApplicationController
   skip_before_action :doorkeeper_authorize!, only: %i[create]
 
   include DoorkeeperRegisterable
+
+  private
+
+  def user_params
+    params.permit(:email, :password, :client_id)
+  end
 end
