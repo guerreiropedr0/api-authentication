80632c120b3139bf94db0fc44df67d4f23b1cba2
Pedro Guerreiro <pmoreiraguerreiro@gmail.com>
2022-07-11 14:14:41 +0100

    Add render_user method that creates a new access token and renders the user

--- app/controllers/concerns/doorkeeper_registerable.rb
+++ app/controllers/concerns/doorkeeper_registerable.rb
@@ -9,4 +9,23 @@ module DoorkeeperRegisterable
       break token unless Doorkeeper::AccessToken.exists?(refresh_token: token)
     end
   end
+
+  def render_user(user, client_app, token_type = 'Bearer')
+    access_token = Doorkeeper::AccessToken.create(
+      resource_owner_id: user.id,
+      application_id: client_app.id,
+      refresh_token: generate_refresh_token,
+      expires_in: Doorkeeper.configuration_access_token_expires_in.to_i,
+      scopes: ''
+    )
+
+    {
+      id: user.id,
+      access_token: access_token.token,
+      token_type:,
+      expires_in: access_token.expires_in,
+      refresh_token: access_token.refresh_token,
+      created_at: access_token.created_at.to_time_to_i
+    }
+  end
 end
