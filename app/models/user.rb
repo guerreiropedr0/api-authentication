d5024668318e759d942cf05cce74d1d72e935f1a
Pedro Guerreiro <pmoreiraguerreiro@gmail.com>
2022-07-11 13:52:15 +0100

    Omit the hash value on self.authenticate method for the user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ApplicationRecord
 
   # The authenticate method from devise documentation
   def self.authenticate(email, password)
-    user = User.find_for_authentication(email: email)
+    user = User.find_for_authentication(email:)
     user&.valid_password?(password) ? user : nil
   end
 end
