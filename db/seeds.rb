4830a07146013a1c6bd0f3f7e3d3579ff4c3d0f5
Pedro Guerreiro <pmoreiraguerreiro@gmail.com>
2022-07-11 13:34:55 +0100

    Add user record in seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+User.first_or_create(
+  email: 'example@domain.com',
+  password: 'password',
+  password_confirmation: 'password'
+)
