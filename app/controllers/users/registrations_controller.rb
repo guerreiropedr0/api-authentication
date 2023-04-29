8d840f9be7319fd3c7043a8e2b48f54aa6945c66
Pedro Guerreiro <pmoreiraguerreiro@gmail.com>
2023-04-29 23:09:59 +0100

    Add better responses to register and session

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -4,6 +4,19 @@ class Users::RegistrationsController < Devise::RegistrationsController
   respond_to :json
   before_action :configure_sign_up_params, only: [:create]
 
+  private
+
+  def respond_with(resource, _opts = {})
+    if resource.persisted?
+      render json: {
+        message: 'Signed up sucessfully.',
+        user: resource
+      }, status: :created
+    else
+      render json: { errors: resource.errors }, status: :unprocessable_entity
+    end
+    en
+
   protected
 
   # If you have extra params to permit, append them to the sanitizer.
--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -2,4 +2,25 @@
 
 class Users::SessionsController < Devise::SessionsController
   respond_to :json
+
+  private
+
+  def respond_with(resource, _opts = {})
+    render json: {
+      message: 'Logged in sucessfully.',
+      user: resource
+    }, status: :ok
+  end
+
+  def respond_to_on_destroy
+    if current_user
+      render json: {
+        message: 'Logged out successfully.'
+      }, status: :ok
+    else
+      render json: {
+        message: 'Couldn\'t find an active session.'
+      }, status: :unauthorized
+    end
+  end
 end
