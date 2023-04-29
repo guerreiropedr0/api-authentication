3330780cb2beafd5fbbfc8600f78f24338e7bbc1
Pedro Guerreiro <pmoreiraguerreiro@gmail.com>
2023-04-29 22:50:12 +0100

    Setup routes and strong params permit

---
+++ app/controllers/users/registrations_controller.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+class Users::RegistrationsController < Devise::RegistrationsController
+  before_action :configure_sign_up_params, only: [:create]
+
+  protected
+
+  # If you have extra params to permit, append them to the sanitizer.
+  def configure_sign_up_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
+end
