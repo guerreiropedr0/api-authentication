b21b4d321062619e3be4856a7be7359a2690f171
Pedro Guerreiro <pmoreiraguerreiro@gmail.com>
2023-04-29 23:10:08 +0100

    Add name validation to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,6 @@ class User < ApplicationRecord
 
   devise :database_authenticatable, :registerable, :validatable,
          :jwt_authenticatable, jwt_revocation_strategy: self
+
+  validates :name, presence: true
 end
